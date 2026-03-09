ec51decf2c78a0a7bad92fa70ab4788d37a6f6d5
Practicum <practicum@ya.ru>
2026-03-09 23:14:42 +0300

    Вроде все готово

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,6 @@ import units.screens.ArchivesScreen
 
 fun main(args: Array<String>) {
     System.setOut(java.io.PrintStream(System.out, true, "UTF-8"))
-    println("хзхз")
     val main_screen = ArchivesScreen()
     main_screen.interact()
 }
